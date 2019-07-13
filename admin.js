5fffbaf97cc140b3730796794402bbc571b478d0
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-13 19:25:11 -0300

    Aqui consta nas rotas a serem usadas.

---
+++ admin.js
@@ -0,0 +1,15 @@
+const express  = require('express')
+const mongoose = require('mongoose')
+const router   = express.Router()
+
+
+//Aqui para carregamento dos models
+require('../models/Aluno') //Aqui estou carregando o model que consta os dados a serem armazenados para alunos
+
+//Aqui será instanciado cada model a ser usado
+const Aluno = mongoose.model('Aluno')
+[]
+
+router.get('/principal', (req, res)=>{
+
+})
