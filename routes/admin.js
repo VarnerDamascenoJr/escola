234a76aebab1301571e49c1dc0a54107ce632f5e
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-16 06:31:00 -0500

    Adicção de tratamentos.

--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,12 @@ const Aluno = mongoose.models('Aluno')
 
 
 router.post('/addaluno', (req, res)=>{
+   
+   var erros = []
+   
+   if(!req.body.nome || typeof req.body.nome == undefined || )
+   
+   
    nome: req.body.nome,
    idade: req.body.idade,
    turno: req.body.turno,
@@ -21,10 +27,14 @@ router.post('/addaluno', (req, res)=>{
    nomePai: req.body.nomePai
 
    new Aluno = aluno.save().then((aluno)=>{
-
+     req.flash('success_msg', 'Aluno adicionado com sucesso!')
+     res.redirect('/')
    }).catch((err)=>{
-     
+     req.flash('error_msg', 'Houve um erro, não foi possível adicionar')
+     res.redirect('/')
    })
 })
+
+
 
 model.exports = router
