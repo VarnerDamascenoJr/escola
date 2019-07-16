b212e50cba621650c6d0418a68044d6a3fb7d8f5
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-15 19:42:40 -0500

    Adição da rota na página inicial

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,14 @@ const router   = express.Router()
 
 
 //Aqui para carregamento dos models
-require('../models/Aluno') //Aqui estou carregando o model que consta os dados a serem armazenados para alunos
+require('./models/Aluno') //Aqui estou carregando o model que consta os dados a serem armazenados para alunos
 
 //Aqui será instanciado cada model a ser usado
 const Aluno = mongoose.model('Aluno')
-[]
 
-router.get('/principal', (req, res)=>{
 
+router.post('/addaluno', (req, res)=>{
+  res.render()
 })
+
+model.exports = router
