5b96e5a9bc7c54d498b9172fe90b1bc1d50acfd5
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-17 19:44:59 -0500

    Correção do acesso ao banco.

--- routes/admin.js
+++ routes/admin.js
@@ -1,40 +1,19 @@
 const express  = require('express')
 const mongoose = require('mongoose')
 const router   = express.Router()
+const Aluno    = require.model('alunos')
 
 
-//Aqui para carregamento dos models
-require('./models/Aluno') //Aqui estou carregando o model que consta os dados a serem armazenados para alunos
 
-//Aqui será instanciado cada model a ser usado
-const Aluno = mongoose.models('Aluno')
+router.get('/',(req, res)=>{
+  res.render('./admin/principal')
+})
 
-
-router.post('/addaluno', (req, res)=>{
+router.post('/aluno', (req, res)=>{
    
-   var erros = []
-   
-   if(!req.body.nome || typeof req.body.nome == undefined || )
-   
-   
-   nome: req.body.nome,
-   idade: req.body.idade,
-   turno: req.body.turno,
-   anoEnsino: req.body.anoEnsino,
-   anoEntrada: req.body.anoEntrada,
-   anoSaida: req.body.anoSaida,
-   nomeMae: req.body.nomeMae,
-   nomePai: req.body.nomePai
-
-   new Aluno = aluno.save().then((aluno)=>{
-     req.flash('success_msg', 'Aluno adicionado com sucesso!')
-     res.redirect('/')
-   }).catch((err)=>{
-     req.flash('error_msg', 'Houve um erro, não foi possível adicionar')
-     res.redirect('/')
-   })
+  
 })
 
 
 
-model.exports = router
+module.exports = router
