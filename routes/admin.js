123fd373c8aa6bf59bfd3e84e1409d17d8919a9c
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-18 17:54:11 -0500

    Adição de rotas e correção de erros.

--- routes/admin.js
+++ routes/admin.js
@@ -6,38 +6,45 @@ require('../models/Aluno')
 
 
 router.get('/',(req, res)=>{
-  res.render('./admin/principal')
+  res.render('./admin')
 })
-
-router.post('/aluno', (req, res)=>{
+//Aqui serão todas as rotas referidas para os alunos.
+router.get("/aluno", (req, res)=>{
+  res.render('./admin/alunocadastro')
+})
+//rota para adição de novo aluno
+router.post('/aluno/novo', (req, res)=>{
  var erros = []
- const novoAluno = {
-   nome: req.body.nome,
-   idade: req.body.idade,
-   turno: req.body.turno,
-   anoEnsino: req.body.anoEnsino,
-   anoEntrada: req.body.anoEntrada,
-   anoSaida: req.body.anoSaida,
-   nomePai: req.body.nomePai,
-   nomeMae: req.body.nomeMae,
-   telefone: req.body.telefone
- }
+
  if (!req.body.nome || typeof req.body.nome == null || req.body.nome == undefined ) { erros.push({texto:"nome inválido"})}
  if (!req.body.nomeMae || typeof req.body.nomeMae == null || req.body.nomeMae == undefined) {erros.push({texto:"Nome da mãe é obrigatório"})}
  if (!req.body.nomePai || typeof req.body.nomePai == null || req.body.nomePai == undefined) {erros.push({texto:"Nome do pai é obrigatório"})}
-  if (!req.body.idade || typeof req.body.idade == null || req.body.idade == undefined ) { erros.push({texto:"Idade é campo obrigatório"})}
+ if (!req.body.idade || typeof req.body.idade == null || req.body.idade == undefined ) { erros.push({texto:"Idade é campo obrigatório"})}
+ if (erros.length > 0) { res.render('/admin/alunoadd') }
+ else {
+   const novoAluno = {
+     nome: req.body.nome,
+     idade: req.body.idade,
+     turno: req.body.turno,
+     anoEnsino: req.body.anoEnsino,
+     anoEntrada: req.body.anoEntrada,
+     anoSaida: req.body.anoSaida,
+     nomePai: req.body.nomePai,
+     nomeMae: req.body.nomeMae,
+     telefone: req.body.telefone
+   }
+   new Aluno(novoAluno).save().then(()=>{
+      req.flash("success_msg", "Aluno cadastrado com sucesso.")
+      res.redirect("/alunoadd")
+   }).catch((err)=>{
+      req.flash("error_msg", "Aluno não cadastrado")
+      res.redirect('/alunoadd')
+   })
+ }
 })
 
-new Aluno(novoAluno).save().then(()=>{
-   req.flash("success_msg", "Aluno cadastrado com sucesso.")
-   res.redirect("/alunoadd")
-}).catch((err)=>{
-   req.flash("error_msg", "Aluno não cadastrado")
-   res.redirect('/alunoadd')
-})
-//A notar que será pego o id exclusivo de cada aluno e então será editado a partir de uma rota post
-router.get('/aluno/edit/:id')
-   Aluno.findOne({_id: req.params.id})
+
+
 
 
 module.exports = router
