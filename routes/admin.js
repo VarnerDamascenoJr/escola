8f8230134aa33b5418f5cead140972015cfb9bad
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-18 19:57:07 -0500

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,8 @@
 const express  = require('express')
 const mongoose = require('mongoose')
 const router   = express.Router()
-const Aluno    = mongoose.model('alunos')
 require('../models/Aluno')
-
+const Aluno    = mongoose.model('alunos')
 
 router.get('/',(req, res)=>{
   res.render('./admin')
@@ -13,15 +12,15 @@ router.get("/aluno", (req, res)=>{
   res.render('./admin/alunocadastro')
 })
 //rota para adição de novo aluno
-router.post('/aluno/novo', (req, res)=>{
+router.post('./aluno/alunoadd', (req, res)=>{
  var erros = []
 
  if (!req.body.nome || typeof req.body.nome == null || req.body.nome == undefined ) { erros.push({texto:"nome inválido"})}
  if (!req.body.nomeMae || typeof req.body.nomeMae == null || req.body.nomeMae == undefined) {erros.push({texto:"Nome da mãe é obrigatório"})}
  if (!req.body.nomePai || typeof req.body.nomePai == null || req.body.nomePai == undefined) {erros.push({texto:"Nome do pai é obrigatório"})}
  if (!req.body.idade || typeof req.body.idade == null || req.body.idade == undefined ) { erros.push({texto:"Idade é campo obrigatório"})}
- if (erros.length > 0) { res.render('/admin/alunoadd') }
- else {
+ if (erros.length > 0) { res.render('/admin/alunoadd', {erros: erros}) }
+
    const novoAluno = {
      nome: req.body.nome,
      idade: req.body.idade,
@@ -35,12 +34,12 @@ router.post('/aluno/novo', (req, res)=>{
    }
    new Aluno(novoAluno).save().then(()=>{
       req.flash("success_msg", "Aluno cadastrado com sucesso.")
-      res.redirect("/alunoadd")
+      res.redirect("admin/alunoadd")
    }).catch((err)=>{
-      req.flash("error_msg", "Aluno não cadastrado")
-      res.redirect('/alunoadd')
+      req.flash("error_msg", "Aluno não cadastrado"+err)
+      res.redirect('admin/alunoadd')
    })
- }
+
 })
 
 
