a2845ce1608bb4d4c77024b47aa17ef291711f29
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-29 17:54:03 -0500

    Entrada de rotas.

--- routes/admin02.js
+++ routes/admin02.js
@@ -2,11 +2,21 @@ const express = require('express')
 const router02 = express.Router()
 
 router02.get('/professor', (req, res)=>{
-  res.render('/admin02/professor')
+  res.render('./admin02/professor')
 })
 
 router02.get('/professor/add', (req, res)=>{
-  res.render('/admin02/professoradd')
+  res.render('./admin02/professoradd')
+})
+
+router02.post('/professor/novo', (req, res)=>{
+  var erros = []
+
+//-------------------rotas para edição-------------------------------------
+
+
+//  -----rotas para remoção de professor ---------------------------------
+
 })
 
 module.exports = router02
