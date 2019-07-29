aff3df05fe2de2cd68c985c017d15ad71d049229
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-07-29 18:07:45 -0500

    Rota para adicionar novo professor.
    Foi adicionado o tratamento para isto.

--- routes/admin02.js
+++ routes/admin02.js
@@ -11,12 +11,17 @@ router02.get('/professor/add', (req, res)=>{
 
 router02.post('/professor/novo', (req, res)=>{
   var erros = []
-
+   if (!req.body.nome || typeof req.body.nome ==undefined || req.body.nome == null ) { erros.push({texto:"Por favor,digite o nome corretamente."})}
+   if (!req.body.telefone || typeof req.body.telefone == undefined || req.body.telefone == null) {erros.push({texto:"Digite o telefone corretamente."})}
+   if (!req.body.disciplina || typeof req.body.disciplina == undefined || req.body.disciplina == null) {erros.push({texto:"Digite a disciplina corretamente."})}
+   if (!req.body.idade || typeof req.body.idade == undefined || req.body.idade == null) {erros.push({texto:"Digite a idade corretamente."}) }
+   else {
+    //Aqui ficará a inserção do professor.
+   }
+})
 //-------------------rotas para edição-------------------------------------
 
 
 //  -----rotas para remoção de professor ---------------------------------
-
-})
 
 module.exports = router02
