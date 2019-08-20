461dedfc022d5a3f6466db995a43960665052a48
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-08-20 13:01:20 -0300

    Model para salvar o usuário.

--- model/Usuario.js
+++ model/Usuario.js
@@ -1,3 +1,25 @@
 const mongoose = require('mongoose')
 const Schema   = mongoose.Schema
 
+const Usuario = New Schema({
+ 
+  nome:{
+   type:String,
+   required:true  
+  },
+  email:{
+   type:String,
+   required:true  
+  },
+  senha:{
+   type:String,
+   required:true 
+  },
+  eAdmin:{
+    type:Number,
+    default:0
+  }
+
+})
+mongoose.model(Usuario,'usuarios')
+
