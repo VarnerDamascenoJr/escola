d37056564da5392636cec75d66eef31e0ba2369d
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-08-20 13:04:52 -0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const routerLogin = express.Router()
-
+//Há certos erros aqui
 routerLogin.get("/login",(req, res)=>{
   res.render('./login/login')
 })
 
 routerLogin.get('/login/principal',(req, res)=>{
   res.render('./login/index')
+})
+
+routerLogin.get('/regsitro', (req, res)=>{
+  res.render('./login/registro)
 })
 
 module.exports = routerLogin
