a2690e9ff2df816d5df4967e43dce78bc26c8b3a
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-08-21 09:10:59 -0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -13,5 +13,15 @@ routerLogin.get('/login/registro', (req, res)=>{
   res.render('./login/registro)
 })
   
-routerLogin.post()
+routerLogin.post('/registrar', (req, res)=>{
+//Aqui consistirá na validação de usuário
+  var erros = []
+
+if(!req.body.nome  || typeof req.body.nome == null  || req.body.nome == undefined) { erros.push({texto:"Campo nome incorreto. Digite corretamente, por favor."})}
+if(!req.body.email || typeof req.body.email == null || req.body.email == undefined){ erros.push({texto:"Campo obrigatório, digite corretamente."})}
+if(!req.body.senha || typeof req.body.senha == null || req.body.senha == undefined){ erros.push({texto:"Digite a senha corretamente."})}
+if(erros.length > 0){
+    res.render("./login/registro", {erros: erros})
+}else{}
+})
 module.exports = routerLogin
