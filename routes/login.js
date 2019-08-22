31e2766443ba444832224b1a3bd92d25f8e0f062
Varner Damasceno <44266193+VarnerDamascenoJr@users.noreply.github.com>
2019-08-22 08:48:06 -0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 require('../models/Usuario')
 const Usuario = mongoose.model('usuarios')
 const routerLogin = express.Router()
-
+const bcrypt    = require('bcryptjs')
 //Há certos erros aqui
 routerLogin.get("/login",(req, res)=>{
   res.render('./login/login')
@@ -28,6 +28,40 @@ if(req.boy.senha != req.body.senha2){ erros.push({texto:"Senhas diferentes.")}
 if(erros.length > 0){
     res.render("./login/registro", {erros: erros})
 }else{
+  
+  Usuario.findOne({email: req.body.email}).then((usuario)=>{
+     //Assim, estou buscando a existência de algum email. Caso ele já exista, será enviada a mensagem de erro
+    if(usuario){ 
+      req.flash("error_msg","Usuário já cadastrado!")
+      res.redirect('./login/registro')
+    }else{
+      const novoUsuario = new Usuario({
+      
+      nome: req.body.nome,
+      email: req.body.email,
+      senha: req.body.senha  
+      })
+      
+     //Aqui haverá o tratamento dado a senha para encriptá-la.
+      bcrypt.genSalt(10,(erro, salt)=>{
+         bcrypt.hash(novoUsuario.senha, salt, (erro, hash)=>{
+            if(erro){
+               req.flash('error_msg', "Houve um erro durante o processo de salvamento.")
+               res.redirect('./login/registro')
+            }
+           
+           novoUsuario.senha = hash
+           novoUsuario.save().then(()=>{
+              req.flash("success_msg", "Usuário salvo com sucesso")
+              res.redirect('/')
+           }).catch((err)=>{
+              req.flash("error_msg","Erro!")
+              res.redirect('./login/registro')
+           })
+         })
+      })
+    }
+  }).catch((err)=>{})
     
 }
 })
